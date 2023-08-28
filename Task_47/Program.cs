ef10752415ad8d6c3216940f9eb66ff5a7b15c9c
Dima Bykadorov <dimka.b@live.com>
2023-08-28 19:13:00 +0300

    Add Task 47

---
+++ Task_47/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введитие количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введитие количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+
+double[,] GetArray(int rows, int columns, int minValue, int maxValue){
+    double[,] result = new double[rows,columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            result[i,j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($" {Math.Round(array[i,j], 1)} ");
+        }
+        Console.WriteLine();
+    }
+}
